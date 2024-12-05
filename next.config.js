e1be125d859a31d45b0be81ac9393a06fa550fc8
Arno Gevorkyan <hello@carty.cc>
2024-12-04 23:16:50 -0500

    domain

--- next.config.js
+++ next.config.js
@@ -6,34 +6,21 @@ const nextConfig = {
       { protocol: "https", hostname: "abs.twimg.com" },
     ],
   },
-};
-
-// next.config.js
-const nextConfiguration = {
-  ...nextConfig,
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
       use: ["@svgr/webpack"],
     });
-
     return config;
   },
-  async redirects() {
-    // remove this redirect in next pearapp release
+  async rewrites() {
     return [
       {
-        source: "/features/tab-autocomplete",
-        destination: "/docs/tab-autocomplete",
-        permanent: true,
-      },
-      {
-        source: "/download",
-        destination: "/pricing",
-        permanent: true,
-      },
+        source: '/home',
+        destination: '/'
+      }
     ];
-  },
+  }
 };
 
-module.exports = nextConfiguration;
+module.exports = nextConfig;
