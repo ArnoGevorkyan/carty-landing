db3fd0254a4cffeea65f689072d3adefbb1bbdd7
Arno Gevorkyan <hello@carty.cc>
2024-12-05 11:11:37 -0500

    data

--- app/providers.tsx
+++ app/providers.tsx
@@ -7,10 +7,21 @@ import { PostHogProvider } from 'posthog-js/react';
 
 export function PHProvider({ children }: { children: React.ReactNode }) {
   useEffect(() => {
-    if (typeof window !== 'undefined') {
+    if (typeof window !== 'undefined' && !window.location.host.includes('localhost')) {
       posthog.init('phc_sZdbmKIdH2qCqiGFHy079fD7766qcgFCLD9bkKeZLDA', {
         api_host: 'https://us.i.posthog.com',
         person_profiles: 'identified_only',
+        capture_pageview: true,
+        capture_pageleave: true,
+        persistence: 'localStorage+cookie',
+        autocapture: true,
+        session_recording: {
+          maskAllInputs: true,
+          maskNetworkRequests: false,
+        },
+        loaded: (posthog) => {
+          if (process.env.NODE_ENV === 'development') posthog.debug();
+        },
       });
     }
   }, []);
