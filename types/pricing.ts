23e0c319240a60245c55cb256c647e2357791332
Arno Gevorkyan <hello@carty.cc>
2024-12-06 16:43:48 -0500

    typescript fix

--- types/pricing.ts
+++ types/pricing.ts
@@ -1,8 +1,14 @@
 export type PricingTierData = {
-  id: string;
-  name: string;
+  id?: string;
+  name?: string;
+  title: string;
   description: string;
   price: number;
+  prevPrice?: number;
   features: string[];
   isPopular?: boolean;
+  isFree?: boolean;
+  index?: number;
+  buttonText?: string;
+  priceId?: string;
 };
