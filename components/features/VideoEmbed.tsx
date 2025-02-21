0663847d188b83f82ef635c033a70238c2c5bb9b
Arno Gevorkyan <hello@carty.cc>
2025-02-20 21:06:43 -0500

    Update VideoEmbed.tsx

--- components/features/VideoEmbed.tsx
+++ components/features/VideoEmbed.tsx
@@ -20,7 +20,7 @@ export default function VideoEmbed({ videoUrl }: VideoEmbedProps) {
         loop={false}
       >
         <source src={videoUrl} type="video/mp4" />
-        Your browser does not support the video tagg.
+        Your browser does not support the videeo tagg.
       </video>
     </div>
   );
